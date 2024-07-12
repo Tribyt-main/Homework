761d1943813f0ba8b2013665c3af94edfa5fa74a
Tribyt-main <bafrilanse@gmail.com>
2024-07-12 13:25:22 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+sum_hw = 12
+sum_min = 90
+clas = 'Python'
+time_per_one = 90/12
+print('Домашних заданий выполнено: ', sum_hw, '* Потрачено минут: ', sum_min)
+print('* Название курса: ', clas, '* Потрачено время на одно задание: ', time_per_one)
