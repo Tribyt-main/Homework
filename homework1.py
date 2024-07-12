1bc6bb84dcef4bd8bcad2c7b8b419e1398bcd187
Tribyt-main <bafrilanse@gmail.com>
2024-07-12 22:23:32 +0300

    # 4st program

--- homework1.py
+++ homework1.py
@@ -1,8 +1,11 @@
-exemple = 'Топинамбур'
+#exemple = 'Топинамбур'
 
-print(exemple[0])
-print(exemple[-1])
-print(exemple[5:])
-print(exemple[::-1])
-print(exemple[1:11:2])
+#print(exemple[0])
+#print(exemple[-1])
+#print(exemple[5:])
+#print(exemple[::-1])
+#print(exemple[1:11:2])
 
+
+# 4st program
+print(int(13.42) == 42.13*100%100) or (int(42.13) == 13.42*100%100)
