37b830c0a8fb5f65b38e81e1c29e5c63f8dc2f86
Tribyt-main <bafrilanse@gmail.com>
2024-07-15 19:11:24 +0300

    homework6.py

---
+++ homework6.py
@@ -0,0 +1,18 @@
+# Библиотека
+my_dict = {'Alek': 08.03, 'Elena': 09.05, 'Marsel': 07.04}
+print(my_dict)
+print(my_dict['Elena'])
+print(my_dict.get('Anton'))
+a = my_dict.pop('Alek')
+print(a)
+my_dict.update({'Oleg': 11.01,
+               'Ilya': 14.02})
+del my_dict['Oleg']
+print(my_dict)
+# Множества
+my_set = [1, 3.5, 7, 2, 8, 5, 'Ok']
+print(my_set)
+my_set = set(my_set)
+my_set.add('no'), my_set.add(27)
+my_set.discard(1)
+print(my_set)
