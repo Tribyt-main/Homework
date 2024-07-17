96834c3fe0bfa01b77272ed12df5b1b283d67d5d
Tribyt-main <bafrilanse@gmail.com>
2024-07-17 13:17:16 +0300

    Update homework6.py

--- homework6.py
+++ homework6.py
@@ -3,12 +3,9 @@ my_dict = {'Alek': 08.03, 'Elena': 09.05, 'Marsel': 07.04}
 print(my_dict)
 print(my_dict['Elena'])
 print(my_dict.get('Anton'))
-a = my_dict.pop('Alek')
-print(a)
 my_dict.update({'Oleg': 11.01,
                'Ilya': 14.02})
-del my_dict['Oleg']
-print(my_dict)
+print(my_dict.pop('Oleg'))
 # Множества
 my_set = [1, 3.5, 7, 2, 8, 5, 'Ok']
 print(my_set)
