7646e88ba2604a6447605588c017bffd1dbf4bf7
Tribyt-main <bafrilanse@gmail.com>
2024-07-21 10:13:12 +0300

    module_2_3.py

---
+++ module_2_3.py
@@ -0,0 +1,17 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+while True:
+    if my_list[0] > 0:
+        print(my_list[0])
+    if my_list[1] > 0:
+        print(my_list[1])
+    if my_list[2] > 0:
+        print(my_list[2])
+    if my_list[3] > 0:
+        print(my_list[3])
+    if my_list[4] > 0:
+        print(my_list[4])
+    if my_list[5] > 0:
+        print(my_list[5])
+    if my_list[6] > 0:
+        print(my_list[6])
+    break
