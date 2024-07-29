5e78a3becec4ad6b3af9573dfbd0c2df53973a24
Tribyt-main <bafrilanse@gmail.com>
2024-07-29 11:11:55 +0300

    module_3_3.py

---
+++ module_3_3.py
@@ -0,0 +1,7 @@
+def print_params(a=1, b='string', c=True):
+    print(a, b, c)
+
+values_list = ['ok', 1, False]
+values_dict = {'a': 2, 'b': 'dict', 'c': False}
+values_list_2 = [1, True]
+print_params(*values_list_2, 42)
