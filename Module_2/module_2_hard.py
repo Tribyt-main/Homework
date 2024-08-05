da637cb2764d15bd4dff0c3ffa19747a2dc3e7ce
Tribyt <retromank@gmail.com>
2024-08-05 21:54:58 +0300

    add file

--- Module_2/module_2_hard.py
+++ Module_2/module_2_hard.py
@@ -1,10 +1,13 @@
 import random
-random_number = random.randint(3, 20)
+random_number = random.randint(15, 15)
 numbers_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-print(random_number)
+a = ''
 for x in numbers_1:
     for i in numbers_1:
-        if x + i == random_number:
-            if x > i:
-                break
-            print(x, i)
+        if random_number % (x + i) == 0 and x < i:
+            a += str(x) + str(i)
+            # if x != i:
+            #     if x > i:
+print(random_number, '-', a)
+
+15 - 1214114232133124115106978
