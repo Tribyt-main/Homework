65c6d31d43135cfdf554450b8c29f6ee4c951619
Tribyt <retromank@gmail.com>
2024-08-06 19:22:15 +0300

    add file

--- Module_2/module_2_hard.py
+++ Module_2/module_2_hard.py
@@ -1,5 +1,5 @@
 import random
-random_number = random.randint(15, 15)
+random_number = random.randint(3, 20)
 numbers_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 a = ''
 for x in numbers_1:
