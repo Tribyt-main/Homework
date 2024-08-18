9ce9081c71220c95d3f3542fa09310c54aa7fb70
Tribyt_man <bafrilanse@gmail.com>
2024-08-18 12:08:26 +0300

    Этажи

--- Reminders/class_human_module_5.py
+++ Reminders/class_human_module_5.py
@@ -2,6 +2,7 @@ class Human:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        self.say_info()
 
     def say_info(self):
         print(f'Привет, меня зовут {self.name}, мне {self.age}')
@@ -10,9 +11,14 @@ class Human:
         self.age += 1
         print(f'У меня день рождения, мне {self.age}')
 
+    def __len__(self):
+        return self.age
+
+    def __del__(self):
+        print(f'{self.name} ушёл')
+
 
 den = Human('Денис', 22)
-max = Human('Макс', 22)
-
-den.birthdey()
-
+max_ = Human('Макс', 22)
+max_.birthdey()
+print(len(den))
