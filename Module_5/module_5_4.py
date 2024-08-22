ef6f60c3f7ddd0ebab6d13fa30b64ff969ebece4
Tribyt_man <bafrilanse@gmail.com>
2024-08-22 20:47:17 +0300

    Добавление метода NEW & DEL

--- Module_5/module_5_4.py
+++ Module_5/module_5_4.py
@@ -2,12 +2,12 @@ class House:
     houses_history = []
 
     def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args)
         return object.__new__(cls)
 
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-        self.houses_history.append(name)
 
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
