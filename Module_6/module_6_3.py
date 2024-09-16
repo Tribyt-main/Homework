c7cdc3a5af1dc5bf1fdfa7104760c56a8ba921f1
Tribyt_main <bafrilanse@gmail.com>
2024-09-16 11:42:10 +0300

    Наследование классов и метод Super

--- Module_6/module_6_3.py
+++ Module_6/module_6_3.py
@@ -1,28 +1,33 @@
 class Hours:
-    x_distance = 0
-    sound = 'Frrr'
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
 
     def run(self, dx):
         self.x_distance += dx
 
 class Eagle:
-    y_distance = 0
-    sound = 'I train, eat, sleep, and repeat'
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
         self.y_distance += dy
 
 
 class Pegasus(Hours, Eagle):
+    def __init__(self):
+        super().__init__()
+        Eagle.__init__(self)
     def move(self, dx, dy):
         super().run(dx)
-        super().fly(dy)
+        self.fly(dy)
 
     def get_pos(self):
-        return f'{self.x_distance}:{self.y_distance}'
+        return (self.x_distance, self.y_distance)
 
     def voice(self):
-        print(super().sound)
+        print(self.sound)
 
 
 p1 = Pegasus()
