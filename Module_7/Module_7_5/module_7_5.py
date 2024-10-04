ae62613fa68685e7694ed3b5372e3e4b36e120f6
Tribyt_main <bafrilanse@gmail.com>
2024-10-04 18:55:02 +0300

    Файлы в ОС "import os"

--- Module_7/Module_7_5/module_7_5.py
+++ Module_7/Module_7_5/module_7_5.py
@@ -1,14 +1,15 @@
 import os
 import time
 
+
 path = os.getcwd()
 for root, dirs, files in os.walk(path):
     for file in files:
-        filepath = path
-        filetime = os.path.getmtime(path)
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(filepath)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
         filesize = os.stat(file).st_size
-        parent_dir = os.path.dirname(path)
+        parent_dir = os.path.dirname(filepath)
         print(f' Обнаружен файл: {file},'
               f'\n Путь: {filepath},'
               f'\n Размер: {filesize} байт,'
