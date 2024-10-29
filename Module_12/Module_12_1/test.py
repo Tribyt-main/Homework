16c211c6d30ab8623ac2c259a825a3c286c8d1fc
Tribyt_main <bafrilanse@gmail.com>
2024-10-29 21:27:18 +0300

    Простые Юнит-Тесты

---
+++ Module_12/Module_12_1/test.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+from Module_12.Module_12_1.runner import runner, runner2
+
+
+class TestRunner(TestCase):
+    def test_walk(self):
+        runner.distance = 0
+        for i in range(0, 10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner.distance = 0
+        for i in range(0, 10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner.distance = 0
+        runner2.distance = 0
+        for i in range(0, 10):
+            runner.walk()
+            runner2.run()
+        self.assertNotEqual(runner.distance, runner2.distance)
