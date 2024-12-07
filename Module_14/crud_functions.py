fbd8122fabc304fbf805c603172e4ef457fa19b6
Tribyt_main <bafrilanse@gmail.com>
2024-12-07 09:58:28 +0300

    Добавление данных из SQL таблицы в ТБ

--- Module_14/crud_functions.py
+++ Module_14/crud_functions.py
@@ -1,5 +1,4 @@
 import sqlite3
-import pprint
 
 connection = sqlite3.connect('initiate_db')
 cursor = connection.cursor()
@@ -13,12 +12,22 @@ def initiate_db():
     description TEXT,
     price INTEGER NOT NULL)
     ''')
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    )
+    ''')
+
     for i in range(1, 5):
         cursor.execute('INSERT INTO Products (title, description, price) VALUES(?,?,?)',
                        (f'Название: Product{i}', f'Описание: описание {i}', f'Цена: {i * 100}'))
-    cursor.execute('DELETE from Products')
+    # cursor.execute('DELETE from Products')
     connection.commit()
-    connection.close()
 
 
 def get_all_products():
@@ -28,7 +37,9 @@ def get_all_products():
         print(i)
 
 
-get_all_products()
+initiate_db()
+
+
 
 cursor.execute('SELECT id, title, description, price FROM Products WHERE id = 1')
 products = cursor.fetchone()
@@ -42,3 +53,25 @@ product_3 = (f'{products[1]} | {products[2]} | {products[3]}')
 cursor.execute('SELECT id, title, description, price FROM Products WHERE id = 4')
 products = cursor.fetchone()
 product_4 = (f'{products[1]} | {products[2]} | {products[3]}')
+
+
+def add_user(username, email, age, balance = 1000):
+    check_user = cursor.execute('SELECT * FROM Users WHERE username =?', (username,))
+    if check_user.fetchone() is None:
+        cursor.execute(f'''
+INSERT INTO Users VALUES('{username}', '{email}' , '{age}', 0)
+''')
+
+
+connection.commit()
+
+
+def is_included(username):
+    check_username = cursor.execute('SELECT * FROM Users WHERE username =?', (username,))
+    if check_username.fetchone() is None:
+        return False
+    else:
+        return True
+
+
+
